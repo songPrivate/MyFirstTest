f23cf86b76962274098cb9bc4891779864c609f3
YT\saulsong <619708455@qq.com>
2017-07-04 17:07:07 +0800

    no message

--- testparent/webservice/src/main/java/Test.java
+++ testparent/webservice/src/main/java/Test.java
@@ -4,6 +4,7 @@
 public class Test {
     void testSout() {
       System.out.println("my Englist name is Saul");
-        System.out.println("my name is songxianda");
+      System.out.println("my name is songxianda");
+      System.out.println("day day up");
     }
 }
