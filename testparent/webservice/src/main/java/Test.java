3d6660a7a5edce5db4052df9d8c0f6b5530e90cf
songPrivate <619708455@qq.com>
2017-07-04 17:04:58 +0800

    Update Test.java

--- testparent/webservice/src/main/java/Test.java
+++ testparent/webservice/src/main/java/Test.java
@@ -3,20 +3,6 @@
  */
 public class Test {
     void testSout() {
-		System.out.println("my shortName is sxd");
-		System.out.println("my name is sxd");   
-		System.out.println("no00");
-        System.out.println("no000");
-        System.out.println("no1");
-        System.out.println("no2");
-        System.out.println("no3");
-        System.out.println("no4");
-        System.out.println("no5");
-        System.out.println("no6");
-        System.out.println("no7");
-        System.out.println("no8");
-        System.out.println("no7");
-        System.out.println("no8");
-        System.out.println("my Englist name is Saul");
+      System.out.println("my Englist name is Saul");
     }
 }
