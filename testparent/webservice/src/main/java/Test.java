67a6b9d40d66aeb4b54d8b3175d7330099cf9255
YT\saulsong <619708455@qq.com>
2017-07-04 16:21:25 +0800

    first file commit test

---
+++ testparent/webservice/src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created by saulsong on 2017/7/4.
+ */
+public class Test {
+    void testSout() {
+        System.out.println("no1");
+        System.out.println("no2");
+        System.out.println("no3");
+        System.out.println("no4");
+        System.out.println("no5");
+    }
+}
