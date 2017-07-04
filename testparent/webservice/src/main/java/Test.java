c73951d9ab6007b0328c1fa68c4d756a3788416e
songPrivate <619708455@qq.com>
2017-07-04 17:41:13 +0800

    Update Test.java

--- testparent/webservice/src/main/java/Test.java
+++ testparent/webservice/src/main/java/Test.java
@@ -3,9 +3,6 @@
  */
 public class Test {
     void testSout() {
-        System.out.println("good good study");
-      System.out.println("my Englist name is Saul");
-      System.out.println("my name is songxianda");
-      System.out.println("day day up");
+      System.out.println("good good study");
     }
 }
