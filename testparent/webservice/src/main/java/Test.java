7ff61a8261a0197426da7649eb32596af9ba453e
YT\saulsong <619708455@qq.com>
2017-07-04 16:48:52 +0800

    no message

--- testparent/webservice/src/main/java/Test.java
+++ testparent/webservice/src/main/java/Test.java
@@ -3,7 +3,7 @@
  */
 public class Test {
     void testSout() {
-        System.out.println("no0");
+        System.out.println("no00");
         System.out.println("no1");
         System.out.println("no2");
         System.out.println("no3");
