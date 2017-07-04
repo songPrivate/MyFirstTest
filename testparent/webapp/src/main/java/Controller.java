ed23649465b7448d274297c046e2d26666188054
YT\saulsong <619708455@qq.com>
2017-07-04 16:29:55 +0800

    第二次提交

---
+++ testparent/webapp/src/main/java/Controller.java
@@ -0,0 +1,31 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by saulsong on 2017/7/4.
+ */
+public class Controller extends HttpServlet {
+
+    private final MyHttpServlet httpServlet = new MyHttpServlet();
+
+    void go() {
+        Test t = new Test();
+        t.testSout();
+
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        go();
+        out.write("test seccussful!");
+        out.flush();
+        out.close();
+    }
+
+    private class MyHttpServlet extends HttpServlet {
+    }
+}
