ddafe3a9cf8fda8bc7138eacff6a18dd23c330d6
YT\saulsong <619708455@qq.com>
2017-07-20 16:52:56 +0800

    dev1.0

--- testparent/webapp/src/main/java/Controller.java
+++ testparent/webapp/src/main/java/Controller.java
@@ -15,7 +15,7 @@ public class Controller extends HttpServlet {
     void go() {
         Test t = new Test();
         t.testSout();
-
+        System.out.printf("dev1");
     }
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
