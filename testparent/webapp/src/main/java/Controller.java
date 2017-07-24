7e2e3fb7045ab7013ebc06d3ba7a916a1880351a
YT\saulsong <619708455@qq.com>
2017-07-24 10:20:33 +0800

    dev12.2

--- testparent/webapp/src/main/java/Controller.java
+++ testparent/webapp/src/main/java/Controller.java
@@ -32,7 +32,8 @@ public class Controller extends HttpServlet {
 		ystem.out.printf("dev10");
         System.out.printf("dev11");
 	    System.out.printf("edit3");
-	    System.out.printf("edit4");
+ System.out.printf("edit4");
+  System.out.printf("dev12");
     }
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
