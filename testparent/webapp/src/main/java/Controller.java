3dca693e672c9ae6a8e71297726ab21370ed0cc9
saulsong <saulsong@yaomaiche.com>
2017-10-19 10:01:13 +0800

    aaa

--- testparent/webapp/src/main/java/Controller.java
+++ testparent/webapp/src/main/java/Controller.java
@@ -19,6 +19,7 @@ public class Controller extends HttpServlet {
         System.out.printf("dev2");
         System.out.printf("onlinefev2");
         System.out.printf("dev3");
+        System.out.printf("dev4");
     }
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
